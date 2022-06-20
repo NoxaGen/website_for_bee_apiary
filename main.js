426c128c1aa86c0284a2e0b4f73f6f24eba1b98d
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-20 17:38:01 +0200

    create javascript file and write some checks

---
+++ main.js
@@ -0,0 +1,6 @@
+const jar1 = document.querySelector('.products .honey .jar1');
+
+jar1.addEventListener('click', function () {
+    window.location.href = "http://net-informations.com";
+    jar1.style.opacity = "0.5";
+})
