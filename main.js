f84ce3758676cf3fc4cbaba906a61828c630d32b
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-21 18:04:25 +0200

    fix variables and do some tests with offsets

--- main.js
+++ main.js
@@ -3,27 +3,29 @@ const jar2 = document.querySelector('.products .honey .jar2');
 const jar3 = document.querySelector('.products .honey .jar3');
 const jar4 = document.querySelector('.products .honey .jar4');
 
+const jar1offset = jar1.scrollTop;
+
 
 jar1.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
-    jar1.style.opacity = "0.5";
-    window.scrollTo(0, 800);
+
+
+    window.scrollTo(0, jar1.offset());
 })
 
 jar2.addEventListener('click', function () {
     window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
-    jar2.style.opacity = "0.5";
+
     window.scrollTo(0, 1800);
 })
 
 jar3.addEventListener('click', function () {
     window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
-    jar3.style.opacity = "0.5";
+
     window.scrollTo(0, 200);
 })
 
 jar4.addEventListener('click', function () {
     window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
-    jar4.style.opacity = "0.5";
+
     window.scrollTo(0, 100);
 })
