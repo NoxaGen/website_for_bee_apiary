7c98f9cfa8061787cbac0f27bc9f1da46cfa6feb
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-21 16:06:33 +0200

    add some subpages for tests

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const jar1 = document.querySelector('.products .honey .jar1');
+const jar2 = document.querySelector('.products .honey .jar2');
+const jar3 = document.querySelector('.products .honey .jar3');
+const jar4 = document.querySelector('.products .honey .jar4');
+
 
 jar1.addEventListener('click', function () {
-    window.location.href = "http://net-informations.com";
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
     jar1.style.opacity = "0.5";
+})
+
+jar2.addEventListener('click', function () {
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    jar2.style.opacity = "0.5";
+})
+
+jar3.addEventListener('click', function () {
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    jar3.style.opacity = "0.5";
+})
+
+jar4.addEventListener('click', function () {
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    jar4.style.opacity = "0.5";
 })
