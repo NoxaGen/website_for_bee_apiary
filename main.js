149451f0fd9562ed177bd82680f33d4ab9d3ce1b
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-21 16:12:36 +0200

    download other elements and do some tests

--- main.js
+++ main.js
@@ -5,21 +5,25 @@ const jar4 = document.querySelector('.products .honey .jar4');
 
 
 jar1.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
     jar1.style.opacity = "0.5";
+    window.scrollTo(0, 800);
 })
 
 jar2.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
     jar2.style.opacity = "0.5";
+    window.scrollTo(0, 1800);
 })
 
 jar3.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
     jar3.style.opacity = "0.5";
+    window.scrollTo(0, 200);
 })
 
 jar4.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products";
+    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
     jar4.style.opacity = "0.5";
+    window.scrollTo(0, 100);
 })
