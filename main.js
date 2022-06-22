30b1a6aecb21594f64f1e73d0d2a20bba72d9150
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-22 17:31:08 +0200

    do some tests with scroll

--- main.js
+++ main.js
@@ -3,29 +3,35 @@ const jar2 = document.querySelector('.products .honey .jar2');
 const jar3 = document.querySelector('.products .honey .jar3');
 const jar4 = document.querySelector('.products .honey .jar4');
 
-const jar1offset = jar1.scrollTop;
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const sep1 = document.getElementById('sep1')
 
 
 jar1.addEventListener('click', function () {
 
 
-    window.scrollTo(0, jar1.offset());
+    window.scrollTo(0, sep1.offsetTop);
 })
 
 jar2.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
 
-    window.scrollTo(0, 1800);
+
+    window.scrollTo(0, 3900);
 })
 
 jar3.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
+
 
     window.scrollTo(0, 200);
 })
 
 jar4.addEventListener('click', function () {
-    window.location.href = "https://noxagen.github.io/pasieka_pod_brzozami/products.html";
+
 
     window.scrollTo(0, 100);
 })
+
+// window.addEventListener('scroll', function () {
+//     console.log(this.scrollY)
+// })
