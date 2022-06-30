8fbc0966739bc54a53c985da648a96c19cdf0ef6
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-30 17:26:39 +0200

    add slider function into javascript

--- main.js
+++ main.js
@@ -1,37 +1,23 @@
-const jar1 = document.querySelector('.products .honey .jar1');
-const jar2 = document.querySelector('.products .honey .jar2');
-const jar3 = document.querySelector('.products .honey .jar3');
-const jar4 = document.querySelector('.products .honey .jar4');
+const sliderMobileImgs = [...document.querySelectorAll('.slider-mobile img')];
+let sliderCounter = 0;
 
-const img1 = document.getElementById('img1');
-const img2 = document.getElementById('img2');
-const sep1 = document.getElementById('sep1')
+setInterval(sliderChanger, 3000)
+
+function sliderChanger() {
 
 
-jar1.addEventListener('click', function () {
+
+    console.log(sliderCounter)
+
+    const changeImage = sliderMobileImgs.findIndex(img => img.classList.contains('img-active'));
+    sliderMobileImgs[changeImage].classList.remove('img-active');
+    sliderCounter++;
+    if (sliderCounter === sliderMobileImgs.length) {
+        sliderCounter = 0;
+    }
+    sliderMobileImgs[sliderCounter].classList.add('img-active');
 
 
-    window.scrollTo(0, sep1.offsetTop);
-})
-
-jar2.addEventListener('click', function () {
 
 
-    window.scrollTo(0, 3900);
-})
-
-jar3.addEventListener('click', function () {
-
-
-    window.scrollTo(0, 200);
-})
-
-jar4.addEventListener('click', function () {
-
-
-    window.scrollTo(0, 100);
-})
-
-// window.addEventListener('scroll', function () {
-//     console.log(this.scrollY)
-// })
+};
