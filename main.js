322e96686bdccca91a49430b682aac2e8abd1ffb
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-07-01 18:22:11 +0200

    quick refactor

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const btnNext = document.querySelector('.slider-mobile .next');
 let sliderCounter = 0;
 let dotsCounter = 0;
 
-
 function nextDot() {
     const changeDot = sliderMobileDots.findIndex(dot => dot.classList.contains('active-dot'));
     sliderMobileDots[changeDot].classList.remove('active-dot');
@@ -14,9 +13,7 @@ function nextDot() {
     if (dotsCounter === sliderMobileDots.length) {
         dotsCounter = 0;
     }
-
     sliderMobileDots[dotsCounter].classList.add('active-dot');
-
 }
 
 function prevDot() {
@@ -29,14 +26,10 @@ function prevDot() {
     }
     const changeDot = sliderMobileDots.findIndex(dot => dot.classList.contains('active-dot'));
     sliderMobileDots[changeDot].classList.remove('active-dot');
-
-
     sliderMobileDots[dotsCounter].classList.add('active-dot');
 }
 
 let autoDots = setInterval(nextDot, 3000)
-
-
 
 function nextSlide() {
     const changeImage = sliderMobileImgs.findIndex(img => img.classList.contains('img-active'));
@@ -46,25 +39,17 @@ function nextSlide() {
         sliderCounter = 0;
     }
     sliderMobileImgs[sliderCounter].classList.add('img-active');
-    console.log('counter is' + sliderCounter)
 };
 
 function prevSlide() {
-
     if (sliderCounter > 0) {
         sliderCounter--;
-        console.log('im swapping on prev fine')
-        console.log('counter is' + sliderCounter)
 
     } else if (sliderCounter <= 0) {
         sliderCounter = sliderMobileImgs.length - 1;
-        console.log('sliderCounter is 0')
-        console.log('counter is' + sliderCounter)
     }
     const changeImage = sliderMobileImgs.findIndex(img => img.classList.contains('img-active'));
     sliderMobileImgs[changeImage].classList.remove('img-active');
-
-
     sliderMobileImgs[sliderCounter].classList.add('img-active');
 }
 
@@ -77,21 +62,19 @@ btnPrev.addEventListener('click', prevArrow);
 function nextArrow() {
     clearInterval(autoDots);
     clearInterval(autoSlide);
-    nextSlide();
-    nextDot();
-
-
-
+    //after 3 seconds of inactivity interval will back to auto
     autoDots = setInterval(nextDot, 3000);
     autoSlide = setInterval(nextSlide, 3000);
+    nextSlide();
+    nextDot();
 }
 
 function prevArrow() {
     clearInterval(autoDots);
     clearInterval(autoSlide);
+    //after 3 seconds of inactivity interval will back to auto
     autoDots = setInterval(nextDot, 3000);
     autoSlide = setInterval(nextSlide, 3000);
     prevSlide();
     prevDot();
-
 }
